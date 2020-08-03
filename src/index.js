37376e3f0c25d076ff5bdffa8b07b189eb428a62
497133212 <497133212@qq.com>
2020-08-03 20:25:48 +0800

    [counter][mandy] feature : wrote a part of redux.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import { createStore } from "redux";
+import { combineReducers } from 'redux';
+const initialCountState = {
+  count: 0
+}
+const initialSumState = {
+  sum: 0
+}
+const INCREASE = 'INCREASE';
+const sumReducer = function (state = initialSumState, action) {
+  return state;
+}
+const countReducer = function (state = initialCountState, action) {
+  switch (action.type) {
+    case INCREASE: {
+      return {
+        ...state,
+        count: [...state.count, action.payload]
+      }
+    }
+    default:
+      return state;
+  }
+}
+const allReduers = {
+  sum: sumReducer,
+  count: countReducer
+}
+const rootReducer = combineReducers(allReduers);
+let store = createStore(rootReducer)
+console.log(store.getState())
+
+function increase(count) {
+  return {
+    type: INCREASE,
+    payload: { count }
+  }
+}
+let unsubscribe = store.unsubscribe(() =>
+  console.log(store.getState())
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.dispatch(INCREASE(1111))
+unsubscribe();
