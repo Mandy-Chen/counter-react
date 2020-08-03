7f20855b186de7c4a48baa779e37c7ec54c037cb
497133212 <497133212@qq.com>
2020-08-03 20:46:37 +0800

    [counter][mandy] feature : add CounterContainer.js

--- src/Components/Counter/index.jsx
+++ src/Components/Counter/index.jsx
@@ -1,42 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
 class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,
-      getcount:0
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       number: 0,
+//       getcount:0
+//     };
+//   }
 
-  increase = () => {
-    this.setState({
-      number: this.state.number + 1,
-    });
-    this.props.parent.getSum(1);
-  };
-  reduce = () => {
-    this.setState({
-      number: this.state.number - 1,
-    });
-    this.props.parent.getSum(-1);
-  };
+//   increase = () => {
+//     this.setState({
+//       number: this.state.number + 1,
+//     });
+//     this.props.parent.getSum(1);
+//   };
+//   reduce = () => {
+//     this.setState({
+//       number: this.state.number - 1,
+//     });
+//     this.props.parent.getSum(-1);
+//   };
 
-  static getDerivedStateFromProps(props,state) {
-    if(props.getcount !== state.getcount) {
-        return {
-          number : 0,
-          getcount : props.getcount
-        }
-    }
-    return null;
-}
+//   static getDerivedStateFromProps(props,state) {
+//     if(props.getcount !== state.getcount) {
+//         return {
+//           number : 0,
+//           getcount : props.getcount
+//         }
+//     }
+//     return null;
+// }
 
   render() {
+    const{value,increase,reduce}=this.props;
     return (
       <div>
-        <button onClick={this.increase}>+</button>
-        <mark>{this.state.number}</mark>
-        <button onClick={this.reduce}>-</button>
+        <button onClick={increase}>+</button>
+        <mark>{value}</mark>
+        <button onClick={reduce}>-</button>
       </div>
     );
   }
---
+++ src/Containers/CounterContainer.js
@@ -0,0 +1,15 @@
+import { connect } from 'reac-redux'
+import Counter from '../Components/Counter'
+import { increaseAction, reduceAction } from '../Actions/Action';
+const mapStateToProps = (state) => ({
+    value: state.value
+})
+const mapDispatchToProps=(dispatchEvent)=>({
+    increase:()=>dispatchEvent(increaseAction()),
+    reduce:()=>dispatchEvent(reduceAction)
+})
+const CountApp=connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter)
+export default CountApp
